42bccff553a4f281c4ab9ff0c364134f3fe68ac4
RanjithRanaRudra <srisasanka096@gmail.com>
2018-10-19 23:48:53 +0530

    commited on 19-10-2018 23:48

--- server/server.test.js
+++ server/server.test.js
@@ -3,26 +3,33 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            message: 'Page Not Found.'
+describe('Server', () => {
+    describe('#GET/', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    message: 'Page Not Found.'
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
-});
-it('should return users response', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res)=> {
-        expect(res.body).toInclude({
-            name: 'Dracula',
-            age: '100M'
+    });
+    describe('#GET/users', () => {
+        it('should return users response', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res)=> {
+                expect(res.body).toInclude({
+                    name: 'Dracula',
+                    age: '100M'
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
+    });
 });
+
